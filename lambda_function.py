d3758e6d8202a317c349af981d64bcd65682b2c5
cargiggles <20098785+cargiggles@users.noreply.github.com>
2018-09-19 17:25:39 -0700

    v1.1
    Changed storage class function and settings. Reduced Redundancy has been deprecated by AWS and is now slightly more expensive than standard class

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
-# Audio Flash Briefing Generator - JSON v1.0
+# Audio Flash Briefing Generator - JSON v1.1
 # Andrew Cargill
 # Sept. 13, 2018
 
@@ -9,16 +9,16 @@ import json
 import uuid
 
 # User Settings
+frugality = True
 single_item_feed = False
-frugality = False
 
 # Lambda Environment Variables
-fb_feed_destination_bucket = os.environ['fb_feed_destination_bucket'] # Ex: flash-briefing-feeds-bucket
-fb_feed_file_name = os.environ['fb_feed_file_name'] # Ex: breaking_news.json
+feed_bucket = os.environ['feed_bucket'] # Ex: flash-briefing-feeds-bucket
+feed_key = os.environ['feed_key'] # Ex: breaking_news.json
 
 # Boto3 Objects
-s3 = boto3.resource("s3")
-bucket = s3.Bucket(fb_feed_destination_bucket)
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(feed_bucket)
 
 def feed_already_exists(bucket, file_name):
     keys =[]
@@ -30,53 +30,55 @@ def feed_already_exists(bucket, file_name):
     else:
         return False
 
+def make_object_public(bucket, key):
+    s3_object = s3.Object(bucket, key.replace('+', ' '))
+    s3_object.Acl().put(ACL = 'public-read')
+
 def make_title_text(object_key):
-    title_text = object_key[object_key.rfind("/")+1:] # Parse key to save only the file name at end
-    title_text = title_text.replace(".mp3", "") # Removes ".mp3" file extension
-    title_text = title_text.replace("+", " ") # Replace "+" with space. Spaces are converted to "+" in S3 trigger
+    title_text = object_key[object_key.rfind('/')+1:] # Parse key to save only the file name at end
+    title_text = title_text.replace('.mp3', '') # Removes ".mp3" file extension
+    title_text = title_text.replace('+', ' ') # Replace "+" with space. Spaces are converted to "+" in S3 trigger
     return title_text
 
-def reduced_redundancy(bucket, file_name):
+def change_storage_class(bucket, key, storage_class):
     copy_source = {
-        "Bucket": bucket,
-        "Key": file_name
+        'Bucket': bucket,
+        'Key': key
     }
-    s3.meta.client.copy(copy_source, bucket, file_name, ExtraArgs = {'StorageClass': 'REDUCED_REDUNDANCY', 'MetadataDirective': 'COPY'})
+    s3.meta.client.copy(copy_source, bucket, key, ExtraArgs = {'StorageClass': storage_class, 'MetadataDirective': 'COPY'})
 
 def lambda_handler(event, context):
-    print event
-    # Should store valuable events here in more friendly variable names for re-use later
+    update_date = event['Records'][0]['eventTime']
+    mp3_bucket = event['Records'][0]['s3']['bucket']['name']
+    mp3_key = event['Records'][0]['s3']['object']['key']
 
-    if feed_already_exists(bucket, fb_feed_file_name) and not single_item_feed:
-        s3.Bucket(fb_feed_destination_bucket).download_file(fb_feed_file_name, "/tmp/existing_feed.json")
-        feed_pointer = "/tmp/existing_feed.json"
+    # Main
+    feed_pointer = '/tmp/feed.json'
+    if feed_already_exists(bucket, feed_key) and not single_item_feed:
+        s3.Bucket(feed_bucket).download_file(feed_key, feed_pointer)
 
-        with io.open(feed_pointer, "r", encoding = "utf-8") as rf:
+        with io.open(feed_pointer, 'r', encoding = 'utf-8') as rf:
             feed = json.loads(rf.read())
 
     else:
-        feed_pointer = "/tmp/new_feed.json"
         feed = []
 
     new_item = {
-        "uid": str(uuid.uuid4()),
-        "updateDate": event['Records'][0]['eventTime'],
-        "titleText": make_title_text(event['Records'][0]['s3']['object']['key']),
-        "mainText": "",
-        "streamUrl": "https://s3.amazonaws.com/" + event['Records'][0]['s3']['bucket']['name'] + "/" + event['Records'][0]['s3']['object']['key']
+        'uid': str(uuid.uuid4()),
+        'updateDate': update_date,
+        'titleText': make_title_text(mp3_key),
+        'mainText': '',
+        'streamUrl': 'https://s3.amazonaws.com/' + mp3_bucket + '/' + mp3_key
     }
 
     if frugality:
-        reduced_redundancy(event['Records'][0]['s3']['bucket']['name'], event['Records'][0]['s3']['object']['key'].replace("+", " "))
+        change_storage_class(mp3_bucket, mp3_key.replace('+', ' '), 'ONEZONE_IA')
 
-    # Making .mp3 file public
-    # Replacing "+" with " " to avoid key error for files with spaces
-    mp3_object = s3.Object(event['Records'][0]['s3']['bucket']['name'], event['Records'][0]['s3']['object']['key'].replace("+", " "))
-    mp3_object.Acl().put(ACL="public-read")
+    make_object_public(mp3_bucket, mp3_key)
 
     feed.insert(0, new_item)
 
-    with io.open(feed_pointer, "w", encoding = "utf-8") as wf:
+    with io.open(feed_pointer, 'w', encoding = 'utf-8') as wf:
         wf.write(json.dumps(feed, indent = 4, sort_keys = True, ensure_ascii = False).decode("utf-8"))
 
-    s3.meta.client.upload_file(feed_pointer, fb_feed_destination_bucket, fb_feed_file_name, ExtraArgs={'ACL': 'public-read','ContentType': "application/json"})
+    s3.meta.client.upload_file(feed_pointer, feed_bucket, feed_key, ExtraArgs = {'ACL': 'public-read','ContentType': 'application/json'})
