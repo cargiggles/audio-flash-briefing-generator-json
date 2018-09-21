93a561a8a0ba0b0844a23cc384f59bbfbf9c1537
cargiggles <20098785+cargiggles@users.noreply.github.com>
2018-09-20 17:12:57 -0700

    v1.2
    Added build_feed function

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
-# Audio Flash Briefing Generator - JSON v1.1
+# Audio Flash Briefing Generator - JSON v1.2
 # Andrew Cargill
 # Sept. 13, 2018
 
@@ -20,12 +20,22 @@ feed_key = os.environ['feed_key'] # Ex: breaking_news.json
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(feed_bucket)
 
-def feed_already_exists(bucket, file_name):
-    keys =[]
-    for key in bucket.objects.all():
-        keys.append(key.key)
+def build_feed(feed, update_date, title_text, bucket, key):
+    new_item = {
+        'uid': str(uuid.uuid4()),
+        'updateDate': update_date,
+        'titleText': title_text,
+        'mainText': '',
+        'streamUrl': 'https://s3.amazonaws.com/' + bucket + '/' + key
+    }
+    feed.insert(0, new_item)
+    return feed
 
-    if file_name in keys:
+def feed_already_exists(bucket, key):
+    keys =[]
+    for s3_object in bucket.objects.all():
+        keys.append(s3_object.key)
+    if key in keys:
         return True
     else:
         return False
@@ -34,8 +44,8 @@ def make_object_public(bucket, key):
     s3_object = s3.Object(bucket, key.replace('+', ' '))
     s3_object.Acl().put(ACL = 'public-read')
 
-def make_title_text(object_key):
-    title_text = object_key[object_key.rfind('/')+1:] # Parse key to save only the file name at end
+def make_title_text(key):
+    title_text = key[key.rfind('/')+1:] # Parse key to save only the file name at end
     title_text = title_text.replace('.mp3', '') # Removes ".mp3" file extension
     title_text = title_text.replace('+', ' ') # Replace "+" with space. Spaces are converted to "+" in S3 trigger
     return title_text
@@ -56,22 +66,12 @@ def lambda_handler(event, context):
     feed_pointer = '/tmp/feed.json'
     if feed_already_exists(bucket, feed_key) and not single_item_feed:
         s3.Bucket(feed_bucket).download_file(feed_key, feed_pointer)
-
         with io.open(feed_pointer, 'r', encoding = 'utf-8') as rf:
             feed = json.loads(rf.read())
-
     else:
         feed = []
 
-    new_item = {
-        'uid': str(uuid.uuid4()),
-        'updateDate': update_date,
-        'titleText': make_title_text(mp3_key),
-        'mainText': '',
-        'streamUrl': 'https://s3.amazonaws.com/' + mp3_bucket + '/' + mp3_key
-    }
-
-    feed.insert(0, new_item)
+    feed = build_feed(feed, update_date, make_title_text(mp3_key), mp3_bucket, mp3_key)
 
     if frugality:
         change_storage_class(mp3_bucket, mp3_key.replace('+', ' '), 'ONEZONE_IA')
@@ -79,6 +79,6 @@ def lambda_handler(event, context):
     make_object_public(mp3_bucket, mp3_key)
 
     with io.open(feed_pointer, 'w', encoding = 'utf-8') as wf:
-        wf.write(json.dumps(feed, indent = 4, sort_keys = True, ensure_ascii = False).decode("utf-8"))
+        wf.write(json.dumps(feed, indent = 4, sort_keys = True, ensure_ascii = False).decode('utf-8'))
 
     s3.meta.client.upload_file(feed_pointer, feed_bucket, feed_key, ExtraArgs = {'ACL': 'public-read','ContentType': 'application/json'})
